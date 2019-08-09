05a3a761634346745972e3a5ce348c301066de98
traviswhittington88 <50853094+traviswhittington88@users.noreply.github.com>
2019-08-09 10:07:52 -0500

    Update index.js
    formatted white-space and indent

--- index.js
+++ index.js
@@ -6,37 +6,32 @@ const tasteDiveApiKey = '341376-TasteFin-9HSMVD5G';
 const youtubeBaseUrl='https://www.googleapis.com/youtube/v3/search';
 const tasteDiveBaseUrl='https://tastedive.com/api/similar';
 
+function displayYoutubeResults(responseJson) {
+  complete();
+  console.log('This is my youtube Api json object',responseJson);
+  
+  $('#js-results-list2').empty();
+  $('#js-results-list2').append(`<h2>Video</h2>`);
 
-
-
-function displayYoutubeResults(responseJson){
-    complete();
-    console.log('This is my youtube Api json object',responseJson);
-    $('#js-results-list2').empty();
-    $('#js-results-list2').append(`<h2>Video</h2>`);
-
-    for (let i = 0; i < responseJson.items.length; i++){
+  for (let i = 0; i < responseJson.items.length; i++) {
     $('#js-results-list2').append(
-        `<li><h3>${responseJson.items[i].snippet.title}</h3>
-        <p>${responseJson.items[i].snippet.description}</p><div class="videowrapper"><iframe src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}?rel=0" frameborder="0" allowfullscreen></iframe></div>
-        </li>`)
+      `<li><h3>${responseJson.items[i].snippet.title}</h3>
+       <p>${responseJson.items[i].snippet.description}</p><div class="videowrapper"><iframe src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}?rel=0" frameborder="0" allowfullscreen></iframe></div>
+       </li>`)
     };
 
-    $('#js-results2').removeClass('hidden');
-    $('.scrollWrapper').removeClass('hidden');
+  $('#js-results2').removeClass('hidden');
+  $('.scrollWrapper').removeClass('hidden');
 }
 
-
-
-
-function youtubeApiFetch(url,options){
-    fetch(url,options)
+function youtubeApiFetch(url,options) {
+  fetch(url,options)
     .then(response => {
-        if(response.ok) {
+      if(response.ok) {
         return response.json();
-        }
-        complete();
-        throw new Error(response.statusText);        
+      }
+      complete();
+      throw new Error(response.statusText);        
     })
     .then(responseJson => displayYoutubeResults(responseJson))
     .catch(err => { 
@@ -44,144 +39,138 @@ function youtubeApiFetch(url,options){
     });
 }
 
-
-function getYoutubeResults(query, maxResults=3){
-   
-    const youtubeParams = {
-        key: youtubeApiKey,
-        q: query,
-        part: 'snippet',
-        maxResults,
-        type: 'video'
-    };
+function getYoutubeResults(query, maxResults=3) {
+  const youtubeParams = {
+    key: youtubeApiKey,
+    q: query,
+    part: 'snippet',
+    maxResults,
+    type: 'video'
+  };
     
-    const queryString = formatQueryParams(youtubeParams);
-    const youtubeUrl = youtubeBaseUrl + '?' + queryString;
-    console.log(youtubeUrl);
+  const queryString = formatQueryParams(youtubeParams);
+  const youtubeUrl = youtubeBaseUrl + '?' + queryString;
+  console.log(youtubeUrl);
 
-    youtubeApiFetch(youtubeUrl); 
+  youtubeApiFetch(youtubeUrl); 
 }
 
-function displayTasteDiveResults(responseJson){
+function displayTasteDiveResults(responseJson) {
   console.log('display results ran');
-    console.log('this is the tastedive object',responseJson);
-    let recommendation = `${mediaType} ${responseJson.Similar.Results[0].Name}`;
-    getYoutubeResults(recommendation);
+  console.log('this is the tastedive object',responseJson);
+    
+  let recommendation = `${mediaType} ${responseJson.Similar.Results[0].Name}`;
+  getYoutubeResults(recommendation);
+    
+  $('#js-results-list1').empty();
 
-    $('#js-results-list1').empty();
-
-    if(responseJson.Similar.Results[0].Type ==='book'){
-      $('#js-results-list1').append(`<li><h2>RECOMMENDATION</h2><p>If you like ${responseJson.Similar.Info[0].Name}...</p><p>You might like the book ${responseJson.Similar.Results[0].Name}.</p><h3>Description</h2><p>${responseJson.Similar.Results[0].wTeaser}</p><p><a href="${responseJson.Similar.Results[0].wUrl}"><h3>Wiki</h3></a></p></li>`);
+  if(responseJson.Similar.Results[0].Type ==='book') {
+    $('#js-results-list1').append(`<li><h2>RECOMMENDATION</h2><p>If you like ${responseJson.Similar.Info[0].Name}...</p><p>You might like the book ${responseJson.Similar.Results[0].Name}.</p><h3>Description</h2><p>${responseJson.Similar.Results[0].wTeaser}</p><p><a href="${responseJson.Similar.Results[0].wUrl}"><h3>Wiki</h3></a></p></li>`);
     $('#js-results1').removeClass('hidden');              
-    }
+  }
 
-    else if(responseJson.Similar.Results[0].Type ==='author'){
-      $('#js-results-list1').append(`<li><h2>RECOMMENDATION</h2><p>If you like ${responseJson.Similar.Info[0].Name}...</p><p>You might like the author ${responseJson.Similar.Results[0].Name}.</p><h3>Description</h2><p>${responseJson.Similar.Results[0].wTeaser}</p><p><a href="${responseJson.Similar.Results[0].wUrl}"><h3>Wiki</h3></a></p></li>`);
+  else if(responseJson.Similar.Results[0].Type ==='author') {
+    $('#js-results-list1').append(`<li><h2>RECOMMENDATION</h2><p>If you like ${responseJson.Similar.Info[0].Name}...</p><p>You might like the author ${responseJson.Similar.Results[0].Name}.</p><h3>Description</h2><p>${responseJson.Similar.Results[0].wTeaser}</p><p><a href="${responseJson.Similar.Results[0].wUrl}"><h3>Wiki</h3></a></p></li>`);
     $('#js-results1').removeClass('hidden');              
-    }
+  }
 
-    else if(responseJson.Similar.Results[0].Type ==='music'){
-      $('#js-results-list1').append(`<li><h2>RECOMMENDATION</h2><p>If you like ${responseJson.Similar.Info[0].Name}...</p><p>You might like the music of ${responseJson.Similar.Results[0].Name}.</p><h3>Description</h2><p>${responseJson.Similar.Results[0].wTeaser}</p><p><a href="${responseJson.Similar.Results[0].wUrl}"><h3>Wiki</h3></a></p></li>`);
+  else if(responseJson.Similar.Results[0].Type ==='music') {
+    $('#js-results-list1').append(`<li><h2>RECOMMENDATION</h2><p>If you like ${responseJson.Similar.Info[0].Name}...</p><p>You might like the music of ${responseJson.Similar.Results[0].Name}.</p><h3>Description</h2><p>${responseJson.Similar.Results[0].wTeaser}</p><p><a href="${responseJson.Similar.Results[0].wUrl}"><h3>Wiki</h3></a></p></li>`);
     $('#js-results1').removeClass('hidden');              
-    }
+  }
 
-    else{
+  else {
     $('#js-results-list1').append(`<li><h2>RECOMMENDATION</h2><p>If you like ${responseJson.Similar.Info[0].Name}...</p><p>You might like the movie ${responseJson.Similar.Results[0].Name}</p><h3>Description</h3><p>${responseJson.Similar.Results[0].wTeaser}
      </p><p><a href="${responseJson.Similar.Results[0].wUrl}"><h3>Wiki</h3></a></p></li>`);
     $('#js-results1').removeClass('hidden');              
-    }
+  }
 }
 
-function tasteDiveApiFetch(url,options){
+function tasteDiveApiFetch(url,options) {
   console.log('fetch ran');
-    load();
-    fetch(url,options)
+  load();
+    
+  fetch(url,options)
     .then(response => {
-        if(response.ok) {
-        return response.json();
-        
-        }
-        throw new Error(response.statusText);       
+      if(response.ok) {
+        return response.json(); 
+      }
+      throw new Error(response.statusText);       
     })
     .then(responseJson => displayTasteDiveResults(responseJson))
     .catch(err => {
-        $('#js-error').removeClass('hidden')
-           if(`${err.message} === Cannot read property 'Name' of undefined`){
-            $('#js-error').text(`That input doesn't exist. Please try again!`);}
-           else{(`Something went wrong: ${err.message}`);}
+      $('#js-error').removeClass('hidden')
+        if(`${err.message} === Cannot read property 'Name' of undefined`) {
+          $('#js-error').text(`That input doesn't exist. Please try again!`);}
+        else {(`Something went wrong: ${err.message}`);}
     });
 }
 
-function formatQueryParams(obj){
-    const queryString = Object.keys(obj)
+function formatQueryParams(obj) {
+  const queryString = Object.keys(obj)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`);
-    console.log(queryString);
-
-    return queryString.join('&');
+  
+  console.log(queryString);
+  return queryString.join('&');
 }
 
-function getTasteDiveResults(query,media){
-    console.log(media);
-    const tasteDiveparams = {
-        q: query,
-        type: media,
-        limit: 1,
-        info: 1,
-        k: tasteDiveApiKey
-    };
-    const tasteDiveQueryString = formatQueryParams(tasteDiveparams);
-    const tasteDiveUrl = herokuUrl + tasteDiveBaseUrl + '?' + tasteDiveQueryString;
-    console.log('this is the tastedive URL',tasteDiveUrl);
+function getTasteDiveResults(query,media) {
+  console.log(media);
+  const tasteDiveparams = {
+    q: query,
+    type: media,
+    limit: 1,
+    info: 1,
+    k: tasteDiveApiKey
+  };
+    
+  const tasteDiveQueryString = formatQueryParams(tasteDiveparams);
+  const tasteDiveUrl = herokuUrl + tasteDiveBaseUrl + '?' + tasteDiveQueryString;
+  console.log('this is the tastedive URL',tasteDiveUrl);
 
-    const tasteDiveOptions = {
-        headers: new Headers({
-                "x-requested-with": "xhr" 
-            })
-                              
-    };
-
-      tasteDiveApiFetch(tasteDiveUrl,tasteDiveOptions);
-
+  const tasteDiveOptions = {
+    headers: new Headers({
+      "x-requested-with": "xhr" 
+    })                        
+  };
+    
+  tasteDiveApiFetch(tasteDiveUrl,tasteDiveOptions);
 }
 
-
-function watchForm(){
-    console.log(`loading watchForm`);
-    $("#js-form").submit(function(event){
-        event.preventDefault();
-        $('#js-error').addClass('hidden');
-        const searchTerm= $('#js-search-term').val();
-        console.log(searchTerm);
-        console.log(mediaType);
-        $("form").trigger("reset");
-        getTasteDiveResults(searchTerm,mediaType);
+function watchForm() {
+  console.log(`loading watchForm`);
+  $("#js-form").submit(function(event) {
+    event.preventDefault();
+      
+    $('#js-error').addClass('hidden');
+    const searchTerm= $('#js-search-term').val();
+      
+    $("form").trigger("reset");
+    getTasteDiveResults(searchTerm,mediaType);
     })
 }
 
-
-function setPlaceholder(mediaType){
-
-        if(mediaType ==='books'){
-            $("#js-search-term").attr('placeholder', 'Enter A Favorite Book');
-        }
-        else if(mediaType==="NULL"){
-           $("#js-search-term").attr('placeholder', 'Enter a book, author, movie or band to get started!');
-        }
-        else if(mediaType ==='authors'){
-            $('#js-search-term').attr('placeholder','How bout your favorite author?');
-        }
-        else if(mediaType==='movies'){
-            $("#js-search-term").attr('placeholder',"e.g.'Pulp Fiction'");
-        }
-        else{
-            $("#js-search-term").attr('placeholder','Pink Floyd');
-        }
+function setPlaceholder(mediaType) {
+  if(mediaType ==='books') {
+    $("#js-search-term").attr('placeholder', 'Enter A Favorite Book');
+  }
+  else if(mediaType==="NULL") {
+    $("#js-search-term").attr('placeholder', 'Enter a book, author, movie or band to get started!');
+  }
+  else if(mediaType ==='authors') {
+    $('#js-search-term').attr('placeholder','How bout your favorite author?');
+  }
+  else if(mediaType==='movies') {
+    $("#js-search-term").attr('placeholder',"e.g.'Pulp Fiction'");
+  }
+  else {
+    $("#js-search-term").attr('placeholder','Pink Floyd');
+  }
 }
 
-
-function getMediaType(){
+function getMediaType() {
   console.log(`loading getMediaType`);
-  $("#js-start").on('click','input[type="button"]',function(event){
+  $("#js-start").on('click','input[type="button"]',function(event) {
         $(event.currentTarget).toggleClass("select");
         $('.js-tab').not(event.currentTarget).removeClass('select').addClass('unselect');
         mediaType= event.currentTarget.id;
@@ -189,36 +178,28 @@ function getMediaType(){
   })
 }
 
-function load(){
-    const loader = $('.loader');
-  
-        loader.css('opacity','1');
-        loader.removeClass('hidden');
-
+function load() {
+  const loader = $('.loader');
+  loader.css('opacity','1');
+  loader.removeClass('hidden');
 }
 
-function complete(){
-    const loader = $('.loader');
-    loader.css('opacity','0')
-    loader.addClass('hidden');
+function complete() {
+  const loader = $('.loader');
+  loader.css('opacity','0')
+  loader.addClass('hidden');
 }
 
-function scroll(){
-    $('.scrollWrapper').click(function(){
-        $('html,body').animate({scrollTop: 0},1000);
-    })
+function scroll() {
+  $('.scrollWrapper').click(function() {
+    $('html,body').animate({scrollTop: 0},1000);
+  })
 }
 
-  function startApp(){
-    scroll();
-    getMediaType();
-    watchForm();
-  }
-   
-
-  
-  
-
-
+function startApp() {
+  scroll();
+  getMediaType();
+  watchForm();
+}
 
 $(startApp);
